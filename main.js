68a40be1c1016f0a1e213758bbb673ad151359a2
Moises Gomes <moisesgomes376@gmail.com>
2022-08-30 22:41:49 -0300

    Adjust scholl behavior

--- main.js
+++ main.js
@@ -4,7 +4,22 @@ window.onload = () => {
     const search = document.querySelector("#options #search")
     const searchInput = document.querySelector("#options #search input")
 
-    expandButton.addEventListener("click", () => aside.classList.toggle("expanded"))
+    expandButton.addEventListener("click", () => {
+        aside.classList.toggle("expanded")
+        if (window.screen.width >= 1024) {
+            if (aside.classList.contains("expanded")) {
+                document.querySelector("main").style.paddingLeft = "25rem"
+            } else {
+                document.querySelector("main").style.paddingLeft = "7.8rem"
+            }
+        } else {
+            if (aside.classList.contains("expanded")) {
+                document.body.style.overflowY = "hidden"
+            } else {
+                document.body.style.overflowY = "initial"
+            }
+        }
+    })
     search.addEventListener("click", () => {
         if (aside.classList.contains("expanded") == false) {
             aside.classList.add("expanded")
